50c57359aaa63e3b49b61574cff6c193e79b581e
Matt Salaciak <matt.salaciak@gmail.com>
2019-02-26 13:29:04 -0500

    Initial Commit

---
+++ Intro/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Intro
+//
+//  Created by Matt Salaciak on 2019-02-26.
+//  Copyright © 2019 Matt Salaciak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
